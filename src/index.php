dd7909efde315cd77aa644f423e8ebca1f83d204
Marshall Miller <contact@marshall-miller.com>
2018-08-10 19:05:45 -0400

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,17 +1,4 @@
-<?php
-	
-	/*
-	|---------------------
-	| Now You Know Central
-	| --------------------
-	|
-	| This is the core of the NYKC site. Anything neccesary for loading the application itself 
-	| and bootstrap settings are done here before the app itself starts.
-	| Requests for pages using the action and subaction parameters come through here and the 
-	| appropriate action is taken depending on those values.
-	|
-	*/
-	
+<?php	
 	//Register the autoloaders.
 	require_once(__DIR__ . "/classes/Autoloader.php"); //All NYKC classes.
 	require_once(__DIR__ . "/vendor/autoload.php"); //third party items like Smarty.
